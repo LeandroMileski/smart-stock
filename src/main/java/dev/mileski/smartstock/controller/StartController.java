0c4e82abe1854d9883330c25fa1317259cdb4e77
Leandro Mileski <leandrotinoco55@gmail.com>
2025-07-06 12:03:07 +0100

    feat: create RestController `StartController.java`

---
+++ src/main/java/dev/mileski/smartstock/controller/StartController.java
@@ -0,0 +1,32 @@
+package dev.mileski.smartstock.controller;
+
+import dev.mileski.smartstock.controller.dto.StartDto;
+import dev.mileski.smartstock.service.SmartStockService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.CompletableFuture;
+
+@RestController
+public class StartController {
+
+    private final SmartStockService smartStockService;
+
+    public StartController(SmartStockService smartStockService) {
+        this.smartStockService = smartStockService;
+    }
+
+    @PostMapping(path = "/start")
+    public ResponseEntity<Void> start(@RequestBody StartDto dto) {
+
+        CompletableFuture.runAsync(() -> {
+                smartStockService.start(dto.reportPath());
+        });
+
+        return ResponseEntity.accepted().build();
+    }
+
+}
+
