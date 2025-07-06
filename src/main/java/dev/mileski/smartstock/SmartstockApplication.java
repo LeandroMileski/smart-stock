2fcdb51cb1bf246f99f50b2b29e5c3bea3c5656e
Leandro Mileski <leandrotinoco55@gmail.com>
2025-07-06 16:08:31 +0100

    feat: insert `@EnableFeignClients`.

--- src/main/java/dev/mileski/smartstock/SmartstockApplication.java
+++ src/main/java/dev/mileski/smartstock/SmartstockApplication.java
@@ -2,9 +2,11 @@ package dev.mileski.smartstock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-public class SmartstockApplication {
+@EnableFeignClients
+	public class SmartstockApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SmartstockApplication.class, args);
