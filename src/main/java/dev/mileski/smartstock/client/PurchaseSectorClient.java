3cd2864b490e4b3832ab522bb938d0e772f135ac
Leandro Mileski <leandrotinoco55@gmail.com>
2025-07-07 16:49:01 +0100

    fix: @RequestHeader and @RequestBody in ResponseEntity.

--- src/main/java/dev/mileski/smartstock/client/PurchaseSectorClient.java
+++ src/main/java/dev/mileski/smartstock/client/PurchaseSectorClient.java
@@ -5,12 +5,15 @@ import dev.mileski.smartstock.client.dto.PurchaseSectorRequest;
 import dev.mileski.smartstock.client.dto.PurchaseSectorResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 
 @FeignClient(name = "purchase-sector-client", url = "${app.purchase-sector.url}")
 public interface PurchaseSectorClient {
 
+    @PostMapping( path = "api/purchases")
     ResponseEntity<PurchaseSectorResponse> sendPurchaseRequest(
             @RequestHeader("Authorization") String token,
-            @RequestHeader PurchaseSectorRequest request);
+            @RequestBody PurchaseSectorRequest request);
 }
